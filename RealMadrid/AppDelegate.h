c4030d33df09b12871dc9c24c1edd17197d15c32
Prjnce <phucnd8x@gmail.com>
2014-03-26 22:44:38 +0700

    add new source code

---
+++ RealMadrid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RealMadrid
+//
+//  Created by Nguyễn Duy Phúc on 3/26/14.
+//  Copyright (c) 2014 Nguyễn Duy Phúc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
